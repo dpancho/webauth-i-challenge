32b207cef7cca8eb73e34a6e0dfd20c9d4ab0476
Dale Pancho <dalepancho7@gmail.com>
2019-11-11 20:17:23 -0800

    MVPx3

--- data/migrations/20191111192542_schema.js
+++ data/migrations/20191111192542_schema.js
@@ -1,14 +1,17 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('users', users => {
-        users.increments();
-        users
-          .string('username', 128)
-          .notNullable()
-          .unique();
-        users.string('password', 128).notNullable();
-      });
+  return knex.schema.createTable('users', tbl =>{
+      tbl.increments()
+      tbl
+        .string('username')
+        .notNullable()
+        .unique()
+      tbl
+        .string('password')
+        .notNullable()
+  })
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('users');
+  return knex.schema
+    .dropTableIfExists('users')
 };
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const server = require('./server')
+const server = require('./api/server.js')
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`\n** Running on port: ${PORT} **\n`)); 
+const port = 4400
+
+server.listen(port, () => {
+    console.log(`listening on port: ${port}`)
+}) 
